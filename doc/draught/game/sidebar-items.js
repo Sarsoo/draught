92cb123b7365411ad2ded5702319f4556c19660c
Sarsoo <Sarsoo@users.noreply.github.com>
2022-02-14 12:14:42 +0000

    deploy: f89fe1ebf67e8364e74e361da8d1d8b15d846bb4

--- doc/draught/game/sidebar-items.js
+++ doc/draught/game/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["Game","Root-level structure for managing the game as a collection of board states"]]});
+initSidebarItems({"constant":[["__wbg_get_game_last_node_count__const",""],["__wbg_get_game_perfect_chance__const",""]],"struct":[["Game","Root-level structure for managing the game as a collection of board states"]]});
