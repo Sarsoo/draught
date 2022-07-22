a30d3005ec748cad2815bf691baaee226ffd910b
Sarsoo <Sarsoo@users.noreply.github.com>
2022-07-22 13:54:42 +0000

    deploy: 8ee1c5a2433a981637005da57b3cdbfa47a0219c

--- doc/source-files.js
+++ doc/source-files.js
@@ -1,3 +1,3 @@
-var N = null;var sourcesIndex = {};
+var sourcesIndex = {};
 sourcesIndex["draught"] = {"name":"","dirs":[{"name":"board","files":["enums.rs","iter.rs","mod.rs"]},{"name":"comp","files":["mod.rs"]},{"name":"game","files":["mod.rs"]}],"files":["lib.rs","paint.rs","utils.rs"]};
 createSourceSidebar();
